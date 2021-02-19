1f3ccc5abe26a8fc328579d3a123936541a050a3
Caleb Wittman <calebjwittman1991@gmail.com>
2021-02-19 09:07:44 -0700

    Reassign variables

--- variable_practice.rb
+++ variable_practice.rb
@@ -4,3 +4,9 @@ float = 4.0
 array = [7, 9, 0, 5]
 boolean = true
 hash = {one: 1, two: 2, three: 3}
+
+p "-----------Re-assign----------"
+
+string = "This is a reassigned string value"
+integer = 8
+float = 5.0
